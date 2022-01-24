3a7d96470b985e4b821d3a9dc20583ea98cd2bf2
Jim Madden <jmadden@twilio.com>
2022-01-24 13:37:29 -0800

    updated get queue

--- queue-wait-time/functions/get-queue-times.js
+++ queue-wait-time/functions/get-queue-times.js
@@ -9,17 +9,11 @@ exports.handler = async (context, event, callback) => {
   const getQueueWaitTime = async (queueSid) => {
     const queueTimes = await getQueueTimes();
     const queueTimesObj = await JSON.parse(queueTimes);
-    console.log('Queue Wait Times: ', queueTimesObj);
     return queueTimesObj.queues[queueSid]['waittime'];
   };
 
-  const response = new Twilio.Response();
-  // Set the status code to 200 OK
-  response.setStatusCode(200);
-  // Set the response body
   const time = await getQueueWaitTime(event.queueSid);
+  console.log('Wait time in seconds: ', time);
 
-  response.setBody(`${time}`);
-
-  return callback(null, response);
+  return callback(null, `${time}`);
 };
